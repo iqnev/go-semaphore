f2378f041541084d28f1315dfb79ea234b07120c
Ivelin Yanev <bgfortran@gmail.com>
2021-05-11 22:45:30 +0300

    add semaphore.go

---
+++ semaphore.go
@@ -0,0 +1,22 @@
+package semaphore
+
+type Semaphore interface {
+	Acquire()
+	Release()
+}
+
+type semaphore struct {
+	semaphoreCount chan  struct{}
+}
+
+func  NewSemaphore(permits int) Semaphore {
+	return &semaphore{semaphoreCount: make(chan struct{}, permits)}
+}
+
+func (s *semaphore) Acquire() {
+	s.semaphoreCount<- struct{}{}
+}
+
+func (s *semaphore) Release() {
+	<-s.semaphoreCount
+}
